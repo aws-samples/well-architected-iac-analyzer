aadafd367a71b7e208e3fe4dfc491b033f44eb2d
Ibrahim Cesar <ibracnb@amazon.com>
2025-10-24 15:15:44 -0300

    fix: adopting change the custom bootstrap to use a already created bucket

--- deploy-wa-analyzer.sh
+++ deploy-wa-analyzer.sh
@@ -359,15 +359,13 @@ deploy_stack() {
     # Bootstrap CDK if needed
     echo "Bootstrapping CDK (if needed) in AWS account $AWS_ACCOUNT and region $REGION..."
     cdk bootstrap \
-        --bootstrap-bucket-name cdk-poc-assets-966676475767-us-east-1 \
-        --qualifier rdsaude \
+        --template custom-bootstrap.yaml
         aws://$AWS_ACCOUNT/$REGION
     
     # Deploy stack
     echo "Deploying stack..."
     cdk deploy \
-        --require-approval never \
-        --context @aws-cdk/core:bootstrapQualifier=rdsaude
+        --require-approval never
     
     # Print post-deployment information
     echo -e "\n📝 Post-Deployment Steps:"
